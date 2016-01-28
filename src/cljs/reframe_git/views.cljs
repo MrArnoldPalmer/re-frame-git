a08f6b8358fa31caebdcb932fe846f4e07799915
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-01-28 00:06:59 -0800

    Started to bind handlers to dom elements in components. Component local atom having issues. Cannot change content of input field for some reason

--- src/cljs/reframe_git/views.cljs
+++ src/cljs/reframe_git/views.cljs
@@ -1,14 +1,21 @@
 (ns reframe-git.views
     (:require [re-frame.core :as re-frame]))
 
-(defn main-panel []
-  (let [name (re-frame/subscribe [:name])]
-    (fn []
-      [:div "Hello from " @name
-        [:div
-          [:button {:on-click #(re-frame/dispatch )}"testing"]]])))
+
+(defn get-repo []
+  (fn []
+    (let [repo-name (atom "")]
+      [:div
+        [:input {:type "text"
+                 :value @repo-name
+                 :on-change #(reset! repo-name (-> % .-target .-value))}]
+        [:button {:on-click #(re-frame/dispatch (:get-repo @repo-name))}
+         "Get Repo"]])))
 
 (defn repo-details []
   (let [repo (re-frame/subscribe [:repo-details])]
     (println repo)
-    [:div (:full_name @repo)]))
+    [:div
+     (:full_name @repo)
+     [get-repo]]))
+
