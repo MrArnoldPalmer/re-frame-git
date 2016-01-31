75ad85816d578a98baa57db7d413a286ca526d9b
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-01-31 15:08:21 -0800

    Added base component for languages tree map

---
+++ src/cljs/reframe_git/components/languages-tree.cljs
@@ -0,0 +1,13 @@
+(ns reframe-git.components.languages-tree
+  (:require [re-frame.core :as re-frame]))
+
+(defn main
+  [languages]
+  (reagent/create-class
+    {:display-name "languages-tree"
+     :component-did-mount
+     #(println "component did mount")
+     :reagent-render
+     (fn [languages]
+       [:div
+        (keys @languages)])}))
--- src/cljs/reframe_git/components/languages.cljs
+++ src/cljs/reframe_git/components/languages.cljs
@@ -1,8 +1,9 @@
 (ns reframe-git.components.languages
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [reframe-git.components.languages-tree :as languages-tree]))
 
 (defn main []
   (let [languages (re-frame/subscribe [:repo-languages])]
     (println @languages)
     [:div
-     (keys @languages)]))
+     (languages-tree/main [languages])]))
