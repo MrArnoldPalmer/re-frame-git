6c87bd0012a2451f5841048823abcc141e2ab72e
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-02-04 17:14:50 -0800

    added file-tree component subscribed to db atom file-tree

---
+++ src/cljs/reframe_git/components/file-tree.cljs
@@ -0,0 +1,15 @@
+(ns reframe-git.components.file-tree
+  (:require [re-frame.core :as re-frame]
+            [reagent.core :as reagent]))
+
+(defn main
+  []
+  (reagent/create-class
+    {:display-name "file-tree"
+     :component-did-mount
+     #(println "file-tree did mount")
+     :reagent-render
+     (fn [tree]
+       (let [tree (re-frame/subscribe [:repo-tree])]
+         [:div
+          (keys @tree)]))}))
--- src/cljs/reframe_git/components/repo-details.cljs
+++ src/cljs/reframe_git/components/repo-details.cljs
@@ -1,11 +1,12 @@
 (ns reframe-git.components.repo-details
     (:require [re-frame.core :as re-frame]
               [reframe-git.components.get-repo :as get-repo]
-              [reframe-git.components.languages :as languages]))
+              [reframe-git.components.languages :as languages]
+              [reframe-git.components.file-tree :as file-tree]))
 
 (defn main []
   (let [repo (re-frame/subscribe [:repo-details])]
     [:div
      (:full_name @repo)
      [get-repo/main]
-     [languages/main]]))
+     [file-tree/main]]))
