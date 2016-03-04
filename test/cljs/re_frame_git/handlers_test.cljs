bca826a58d80f81755c18f56055ffb79ba38b407
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-03-03 19:55:25 -0800

    wrote tests for all existing handler functions. process-repo-branches-response test failing due to type error

--- test/cljs/re_frame_git/handlers_test.cljs
+++ test/cljs/re_frame_git/handlers_test.cljs
@@ -23,14 +23,57 @@
         (is (= (get-in response [:options 1 :response-format]) :json))
         (is (= (get-in response [:options 1 :keywords?]) true))))))
 
+(deftest api-error
+  (testing "prints error to console, returns map parameter as passed in"
+    (let [db {:test "test"}
+          result (handlers/api-error db [_ "response"])]
+      (is (= result db)))))
+
+(deftest get-repo
+  (testing "returns map as passed in"
+    (let [db {:test "test"}
+          result (handlers/get-repo db [_ _])]
+      (is (= result db)))))
+
 (deftest process-repo-response
   (testing "returns MAP with response maps associated with :repo-details in map parameter"
     (let [repo-details "repo-details"
           db (handlers/process-repo-response {} [_ _ repo-details])]
       (is (= (:repo-details db) repo-details)))))
 
-(deftest process-repo-error
-  (testing "prints error to console, returns map parameter as passed in"
+(deftest get-repo-languages
+  (testing "returns map as passed in"
     (let [db {:test "test"}
-          result (handlers/process-repo-error db)]
+          result (handlers/get-repo-languages db [_ _])]
       (is (= result db)))))
+
+(deftest process-repo-languages-response
+  (testing "returns map with response maps associated with :repo-languages in map parameter"
+    (let [repo-languages "repo-languages"
+          db (handlers/process-repo-languages-response {} [_ _ repo-languages])]
+      (is (= (:repo-languages db) repo-languages)))))
+
+(deftest get-repo-branches
+  (testing "returns map as passed in"
+    (let [db {:test "test"}
+          result (handlers/get-repo-branches db [_ _])]
+      (is (= result db)))))
+
+(deftest process-repo-branches-response
+  (testing "returns map with response maps associated with :repo-branches in map parameter"
+    (let [repo-branches "repo-branches"
+          db (handlers/process-repo-branches-response {} [_ _ repo-branches])]
+      (is (= (:repo-branches db) repo-branches)))))
+
+(deftest get-repo-tree
+  (testing "returns map as passed in"
+    (let [db {:test "test"}
+          result (handlers/get-repo-tree db [_ _])]
+      (is (= result db)))))
+
+(deftest process-repo-tree-response
+  (testing "returns map with response maps associated with :repo-tree in map parameter"
+    (let [repo-tree "repo-tree"
+          db (handlers/process-repo-tree-response {} [_ _ repo-tree])]
+      (is (= (:repo-tree db) repo-tree)))))
+
