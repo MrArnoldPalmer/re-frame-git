ef30722cc1e61867d778cf6bcd4bd62a7a834d5b
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-03-04 10:53:23 -0800

    added file-tree-graph-testing namespace

--- test/cljs/re_frame_git/runner.cljs
+++ test/cljs/re_frame_git/runner.cljs
@@ -1,7 +1,9 @@
 (ns re-frame-git.runner
     (:require [doo.runner :refer-macros [doo-tests]]
               [re-frame-git.core-test]
-              [re-frame-git.handlers-test]))
+              [re-frame-git.handlers-test]
+              [re-frame-git.components.file-tree-graph-test]))
 
 (doo-tests 're-frame-git.core-test
-           're-frame-git.handlers-test)
+           're-frame-git.handlers-test
+           're-frame-git.components.file-tree-graph-test)
