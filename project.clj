e071732ed162f114e4904a55d22976cb48120afb
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-03-04 17:30:56 -0800

    started messing with ds tree map functions and JS interop stuff

--- project.clj
+++ project.clj
@@ -1,17 +1,18 @@
 (defproject re-frame-git "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
-                 [reagent "0.5.1"]
-                 [re-frame "0.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [reagent "0.6.0-alpha"]
+                 [re-frame "0.7.0-alpha-3"]
                  [cljs-ajax "0.5.3"]
-                 [garden "1.3.0"]]
+                 [garden "1.3.2"]
+                 [cljsjs/d3 "3.5.7-1"]]
 
   :min-lein-version "2.5.3"
 
   :source-paths ["src/clj"]
 
-  :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0-2"]
+  :plugins [[lein-cljsbuild "1.1.2"]
+            [lein-figwheel "0.5.0-6"]
             [lein-garden "0.2.6"]
             [lein-doo "0.1.6"]]
 
@@ -42,7 +43,10 @@
                                    :output-to "resources/public/js/compiled/app.js"
                                    :output-dir "resources/public/js/compiled/out"
                                    :asset-path "js/compiled/out"
-                                   :source-map-timestamp true}}
+                                   :optimizations :none
+                                   :source-map true
+                                   :source-map-timestamp true
+                                   :verbose true}}
 
                        {:id "test"
                         :source-paths ["src/cljs" "test/cljs"]
--- src/cljs/re_frame_git/components/file-tree-graph.cljs
+++ src/cljs/re_frame_git/components/file-tree-graph.cljs
@@ -1,7 +1,8 @@
 (ns re-frame-git.components.file-tree-graph
    (:require [clojure.string :refer [split]]
              [re-frame.core :as re-frame]
-             [reagent.core :as reagent]))
+             [reagent.core :as reagent]
+             [cljsjs.d3]))
 
 (defn build-file-map
   [file-name info]
@@ -26,29 +27,73 @@
                       (into [:root]))
        :type "directory"})))
 
-(defn format-tree-map
+(defn format-tree-map-data
   [tree-data]
   (reduce (fn [formatted-map item]
             (let [formatted-item (format-item item)]
               (if (= (:type formatted-item) "directory")
-                (assoc-in formatted-map (:location formatted-item) {:files []})
+                (assoc-in formatted-map (:location formatted-item) {:children []})
                 (let [file (:details formatted-item)
-                      directory (conj (:location formatted-item) :files)]
+                      directory (conj (:location formatted-item) :children)]
                   (assoc-in formatted-map directory (conj (get-in formatted-map directory) file))))))
           {}
           (:tree tree-data)))
+
+(defn build-tree-map
+  []
+  (-> js/d3
+      (.-layout)
+      (.treemap)
+      (.size [500 500])
+      (.sticky true)
+      (.value (fn [d] (.-size d)))))
+
+(defn position
+  [this]
+  (println this)
+  (-> this
+      (.style "left" (fn [d] (str (.-x d) "px")))
+      (.style "top" (fn [d] (str (.-x d) "px")))
+      (.style "width" (fn [d] (str (max 0, (- (.-dx d) 1)) "px")))
+      (.style "height" (fn [d] (str (max 0, (- (.-dy d) 1)) "px")))))
+
+(defn mount-tree-map
+  [tree-map element data]
+  (let [div (-> js/d3
+                (.select element)
+                (.append "div")
+                (.style "position" "relative")
+                (.style "width" "500px")
+                (.style "height" "500px"))
+        node (-> div
+                 (.datum data)
+                 (.selectAll ".node")
+                 (.data (.-nodes tree-map))
+                 (.enter)
+                 (.append "div")
+                 (.attr "class" "node")
+                 (.style "background" "blue")
+                 (.style "position" "absolute")
+                 (.style "width" "100%")
+                 (.style "height" "100%")
+                 (.text (fn [d] (.-name d))))]
+    (println div)
+    (println node)))
 
 (defn main
   [tree-graph-data]
   (reagent/create-class
     {:display-name "file-tree"
+     :component-did-update
+     (fn [tree-graph-data]
+       (println "update")
+       (let [tree-map (build-tree-map)]
+         (mount-tree-map tree-map "#file-tree-graph" (format-tree-map-data tree-graph-data))))
      :component-did-mount
      (fn []
-       (-> tree-graph-data
-           format-tree-map
-           println))
+       (let [tree-map (build-tree-map)]
+         (mount-tree-map tree-map "#file-tree-graph" (format-tree-map-data tree-graph-data))))
      :reagent-render
      (fn [tree-graph-data]
-       [:div
-        (keys tree-graph-data)])}))
+       [:div#file-tree-graph])}))
 
