828c414e3bc743f951f536f203d6ec6ed92f2e83
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-03-12 14:59:09 -0800

    starting over with format-tree-map-data reducer

--- src/cljs/re_frame_git/components/file-tree-graph.cljs
+++ src/cljs/re_frame_git/components/file-tree-graph.cljs
@@ -60,58 +60,13 @@
                  (.append "div")
                  (.style "font-size" compute-font-size))]))
 
-(def test-data
-  {:name "test"
-   :children [{:name "again" :size 100} {:name "again2" :size 500} {:name "nested" :children [{:name "nested file" :size 350}]}]})
-
-(defn format-file-map
-  [item]
-  (let [file-structure (split (:path item) "/")
-        file (last file-structure)
-        path (into ["root"] (drop-last file-structure))]
-    {:location path
-     :name file
-     :size (:size item)}))
-
-(defn deep-merge [formatted-map item-map]
-  (merge-with (fn [x y]
-                (println x y)
-                (cond (vector? y) (into x y) 
-                      (map? y) (deep-merge x y)
-                      :else y)) 
-                 formatted-map item-map))
-
-(defn merge-directory
-  [formatted-map item]
-  (let [path-vector (:location item)
-        nested-item (reduce (fn [formatted-item path]
-                              (if (empty? formatted-item)
-                                [{:name (:name item) :size (:size item)}]
-                                {:name path :children formatted-item}))
-                            []
-                            path-vector)]
-    (deep-merge formatted-map {:name "root" :children nested-item})))
-
-(defn format-tree-map-data
-  [tree-graph-data]
-  (->> tree-graph-data
-       (:tree)
-       (filter #(= (:type %1) "blob"))
-       (map format-file-map)
-       ;(sort-by #(count (:location %1)))
-       (reduce (fn [formatted-map item]
-                 (merge-directory formatted-map item))
-               {:name "root" :children[]})))
 (defn main
   [tree-graph-data]
   (reagent/create-class
     {:display-name "file-tree"
      :component-did-mount
      (fn []
-       (println "mount")
-       (let [tree-map-data (format-tree-map-data tree-graph-data)]
-         (println tree-map-data)
-         (render-tree-map tree-map-data)))
+       )
      :reagent-render
      (fn [tree-graph-data]
        [:div#file-tree-graph])}))
