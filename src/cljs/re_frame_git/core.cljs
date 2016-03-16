1dae1be3fc1d2cd3b55f7a8a2dce3b3bc61d8c96
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-03-15 20:32:24 -0700

    cleaned up mount-root

--- src/cljs/re_frame_git/core.cljs
+++ src/cljs/re_frame_git/core.cljs
@@ -3,7 +3,7 @@
               [re-frame.core :as re-frame]
               [re-frame-git.handlers]
               [re-frame-git.subs]
-              [re-frame-git.components.core :as application]
+              [re-frame-git.components.core :refer [main]]
               [re-frame-git.config :as config]
               [reagent-dev-tools.core :as dev-tools]
               [reagent-dev-tools.state-tree :as dev-state]))
@@ -14,15 +14,14 @@
 (when config/debug?
   (dev-state/register-state-atom "App" re-frame.db/app-db))
 
-(if config/debug?
-  (do
-    (defn mount-root []
-      (reagent/render [:div [application/main] [dev-tools/dev-tool {}]]
-                      (.getElementById js/document "app"))))
-  (do
-    (defn mount-root []
-      (reagent/render [application/main]
-                      (.getElementById js/document "app")))))
+(defn mount-root []
+  (if config/debug?
+    (reagent/render [:div
+                     [main]
+                     [dev-tools/dev-tool {}]]
+                    (.getElementById js/document "app"))
+    (reagent/render [main]
+                    (.getElementById js/document "app"))))
 
 (defn ^:export init [] 
   (re-frame/dispatch-sync [:initialize-db])
