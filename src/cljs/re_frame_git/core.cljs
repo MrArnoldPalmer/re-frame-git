cbb59a9d8a9e2e6fb83c23e7d0011dec83f579d4
Mitchell Valine <mitchellvaline@yahoo.com>
2016-03-19 13:32:41 -0700

    Revert "reagent-dev-tools not required in core namespace."
    This reverts commit 3ac1f95272fea93be9eb7fa4988e24aa6d2e1852.

--- src/cljs/re_frame_git/core.cljs
+++ src/cljs/re_frame_git/core.cljs
@@ -5,18 +5,19 @@
               [re-frame-git.subs]
               [re-frame-git.components.core :refer [main]]
               [re-frame-git.config :as config]))
+              ;[reagent-dev-tools.core :as dev-tools]
+              ;[reagent-dev-tools.state-tree :as dev-state]))
 
 (when config/debug?
   (println "dev mode"))
 
 (when config/debug?
-  (reagent-dev-tools.state-tree/register-state-atom "App" re-frame.db/app-db))
+  (dev-state/register-state-atom "App" re-frame.db/app-db))
 
 (defn mount-root []
   (if config/debug?
     (reagent/render [:div
-                     [main]
-                     [reagent-dev-tools.core/dev-tool {}]]
+                     [main]]
                     (.getElementById js/document "app"))
     (reagent/render [main]
                     (.getElementById js/document "app"))))
