007083989a772c8da398d86737ef63b4594de33b
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-05-04 14:23:35 -0700

    Fixed warning for undeclared variable used in handlers/core test

--- test/cljs/re_frame_git/handlers/core_test.cljs
+++ test/cljs/re_frame_git/handlers/core_test.cljs
@@ -5,12 +5,12 @@
 (deftest set-current-route
   (testing "sets :current-route key in app state to route string parameter"
     (let [route-name "route-string"
-          result (handlers/set-current-route {} [_ route-name])]
+          result (handlers/set-current-route {} [nil route-name])]
       (is (= (:current-route result) route-name)))))
 
 (deftest api-error
   (testing "prints error to console, returns map parameter as passed in"
     (let [db {:test "test"}
-          result (handlers/api-error db [_ "response"])]
+          result (handlers/api-error db [nil "response"])]
       (is (= result db)))))
 
