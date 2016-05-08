b8cca270e4a32f1ac6894bc4eb4185acb6a83d23
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-05-08 15:35:56 -0700

    changed repo-details to repo-details-container

--- src/cljs/re_frame_git/components/core.cljs
+++ src/cljs/re_frame_git/components/core.cljs
@@ -1,7 +1,7 @@
 (ns re-frame-git.components.core
   (:require [re-frame.core :as re-frame]
-            [re-frame-git.components.repo-details :refer [repo-details]]))
+            [re-frame-git.components.repo-details-container :refer [repo-details-container]]))
 
 (defn main []
   [:div
-   [repo-details]])
+   [repo-details-container]])
