2ee0c5e27effe3f25201eab42ffdb7c8f027c5a0
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-05-13 13:02:32 -0700

    added test file for utils.core

--- test/cljs/re_frame_git/runner.cljs
+++ test/cljs/re_frame_git/runner.cljs
@@ -1,13 +1,13 @@
 (ns re-frame-git.runner
     (:require [doo.runner :refer-macros [doo-tests]]
               [re-frame-git.core-test]
-              [re-frame-git.helpers-test]
+              [re-frame-git.utils.core-test]
               [re-frame-git.handlers.core_test]
               [re-frame-git.handlers.repo-handlers-test]
               [re-frame-git.components.file-tree-graph-test]))
 
 (doo-tests 're-frame-git.core-test
-           're-frame-git.helpers-test
+           're-frame-git.utils.core-test
            're-frame-git.handlers.core-test
            're-frame-git.handlers.repo-handlers-test
            're-frame-git.components.file-tree-graph-test)
---
+++ test/cljs/re_frame_git/utils/core_test.cljs
@@ -0,0 +1,14 @@
+(ns re-frame-git.utils.core-test
+  (:require [cljs.test :refer-macros [deftest testing is]]
+            [re-frame-git.utils.core :as utils]))
+
+(deftest GET
+  (testing "Makes GET request using cljs-http and core.async and returns response body"))
+
+(deftest item-loaded
+  (testing "returns false with map argument which holds nil for value of any existing key"
+    (let [result (utils/item-loaded {:a nil :b nil :c "string"})]
+      (is (= result false))))
+  (testing "returns true with map argument whose values for all existing keys are not nil"
+    (let [result (utils/item-loaded {:a "string" :b 2 :c []})]
+      (is (= result true)))))
